a86a9e9a1c2f0b7283386b858c909434156d60f8
Fahim Faisal <fahimfaisal1998@gmail.com>
2022-12-06 18:08:44 +0600

    Starting Backend

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const port = process.env.PORT || 8080;
+var corsOrigin = {
+  origin: ["http://localhost:3000","https://pass.fahimfaisal.net"],
+};
+app.use(cors(corsOrigin));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () =>
+  console.log(`Server Started :: http://localhost:${port}`)
+);
+
 const Profiles = require("./model/profiles");
 mongoose.connect(
   "mongodb+srv://fahim2204:fahim2204@cluster0.md64krj.mongodb.net/camouflagedb",
